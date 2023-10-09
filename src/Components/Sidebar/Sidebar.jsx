01e4c619eac4e14cf449d0260e1b6dd05f48d7d7
AmirHosein-Keimasi <76690930+AmirHosein-Keimasi@users.noreply.github.com>
2023-10-10 00:00:49 +0330

    Refactoring Tabs

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -4,13 +4,6 @@ import SidebarDrawer from '../drawer/SidebarDrawer'
 
 
 const Sidebar = () => {
-  // const tabs =[
-  //   {label:"صفحه اصلی" , icon :<HomeRounded/>, ...tabProps(0)},
-  //   {label:"صفحه اصلی" , icon :<FaceRounded/>, ...tabProps(1)},
-  //   {label:"صفحه اصلی" , icon :<TextSnippetRounded/>, ...tabProps(2)},
-  //   {label:"صفحه اصلی" , icon :<TerminalRounded/>, ...tabProps(3)},
-  //   {label:"صفحه اصلی" , icon :<MessageRounded/>, ...tabProps(4)},
-  // ]
 
   return (
     <>
--- src/Components/Sidebar/SidebarTabs.jsx
+++ src/Components/Sidebar/SidebarTabs.jsx
@@ -1,28 +1,17 @@
-import {
-  FaceRounded,
-  Home,
-  MessageRounded,
-  TerminalRounded,
-  TextSnippetRounded,
-} from "@mui/icons-material";
 import { Tab, Tabs } from "@mui/material";
 import { useContext } from "react";
 import MainContext from "../../Context";
-
+import { tabsData } from "../data/tabsData";
 
 const SidebarTabs = () => {
-    const {  setDrawerOpen,handelPageNumber,pageNumber } = useContext(MainContext);
-
-  const tabProps = (index) => {
-    return {
-      id: `sidebar-tab-${index}`,
-      "aria-controls": `tabpanel-${index}`,
-    };
-  };
+  const { setDrawerOpen, handelPageNumber, pageNumber } =
+    useContext(MainContext);
 
 
+    const data=tabsData()
   return (
     <>
+
       <Tabs
         orientation="vertical"
         variant="scrollabel"
@@ -31,76 +20,25 @@ const SidebarTabs = () => {
         onChange={handelPageNumber}
         value={pageNumber}
       >
-        <Tab
-          label="صفحه اصلی"
-          sx={{
-            my: 0.5,
-            mx: 1,
-            "&.MuiTab-root": {
-              minHeight: 50,
-            },
-          }}
-          icon={<Home />}
-          iconPosition="start"
-          {...tabProps(0)}
-          onClick={()=>{setDrawerOpen(false)}}
-        />
-        <Tab
-          sx={{
-            my: 0.5,
-            mx: 1,
-            "&.MuiTab-root": {
-              minHeight: 50,
-            },
-          }}
-          {...tabProps(1)}
-          label="درباره ی من "
-          icon={<FaceRounded />}
-          iconPosition="start"
-          onClick={()=>{setDrawerOpen(false)}}
-        />
-        <Tab
-          {...tabProps(2)}
-          label="رزومه من"
-          icon={<TextSnippetRounded />}
-          iconPosition="start"
-          onClick={()=>{setDrawerOpen(false)}}
-          sx={{
-            my: 0.5,
-            mx: 1,
-            "&.MuiTab-root": {
-              minHeight: 50,
-            },
-          }}
-        />
-        <Tab
-          {...tabProps(3)}
-          label="نمونه کار ها "
-          icon={<TerminalRounded />}
-          iconPosition="start"
-          onClick={()=>{setDrawerOpen(false)}}
-          sx={{
-            my: 0.5,
-            mx: 1,
-            "&.MuiTab-root": {
-              minHeight: 50,
-            },
-          }}
-        />
-        <Tab
-          {...tabProps(4)}
-          label=" ارتباط با من"
-          icon={<MessageRounded />}
-          iconPosition="start"
-          onClick={()=>{setDrawerOpen(true)}}
-          sx={{
-            my: 0.5,
-            mx: 1,
-            "&.MuiTab-root": {
-              minHeight: 50,
-            },
-          }}
-        />
+        {data.map((tab, index) => (
+          <Tab
+            key={index}
+            {...tab}
+            icon={tab.icon}
+            label={tab.label}
+            sx={{
+              my: 0.5,
+              mx: 1,
+              "&.MuiTab-root": {
+                minHeight: 50,
+              },
+            }}
+            iconPosition="start"
+            onClick={() => {
+              setDrawerOpen(false);
+            }}
+          />
+        ))}
       </Tabs>
     </>
   );
